1efca714a25983bc459a5693980e3306633b2d72
noc291001 <=>
2021-12-28 15:19:14 +0700

    Add graphql

--- cmd/api/middleware.go
+++ cmd/api/middleware.go
@@ -13,7 +13,7 @@ import (
 func (app *Application) enableCORS(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Access-Control-Allow-Origin", "*")
-		w.Header().Set("Access-Control-Allow-Headers", "Content-Type,Authorization")
+		// w.Header().Set("Access-Control-Allow-Headers", "Content-Type,Authorization")
 		next.ServeHTTP(w, r)
 	})
 }
--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -20,6 +20,7 @@ func (app *Application) routes() http.Handler {
 	secure := alice.New(app.checkToken)
 
 	router.HandlerFunc(http.MethodGet, "/status", app.statusHandler)
+	router.HandlerFunc(http.MethodPost, "/v1/graphql/list", app.moviesGraphQL)
 	router.HandlerFunc(http.MethodPost, "/v1/signin", app.SignIn)
 
 	router.HandlerFunc(http.MethodGet, "/v1/movie/:id", app.getOneMovie)
@@ -30,9 +31,6 @@ func (app *Application) routes() http.Handler {
 
 	router.POST("/v1/admin/editmovie", app.wrap(secure.ThenFunc(app.editMovie)))
 	router.GET("/v1/admin/deletemovie/:id", app.wrap(secure.ThenFunc(app.deleteMovie)))
-
-	// router.HandlerFunc(http.MethodPost, "/v1/admin/editmovie", app.editMovie)
-	// router.HandlerFunc(http.MethodGet, "/v1/admin/deletemovie/:id", app.deleteMovie)
 
 	return app.enableCORS(router)
 }
